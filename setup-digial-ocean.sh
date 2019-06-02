72ee671f7bcb3a93d02dda203f77b5ef2490bf4f
Yang ChenMing <yang.chenming@oaknorth.ai>
2019-06-02 15:14:22 +0800

    test both ubuntu and centos vpn script, under ubuntu 16.04 64bits, and centos 7.6 latest

--- setup-digial-ocean.sh
+++ setup-digial-ocean.sh
@@ -1,8 +1,11 @@
-ansible-playbook -i hosts -u root playbook-by-role-ping.yml
+#ansible-playbook -i hosts -u root playbook-by-role-ping.yml
 ansible-playbook -i hosts -u root playbook-by-role-basic.yml
 # ansible-playbook -i hosts -u root playbook-by-role-basic.yml -vv
 
 # only run when needed
-# ansible-playbook -i hosts -u root playbook-by-role-docker.yml
-# ansible-playbook -i hosts -u root playbook-by-role-nginx.yml
+ansible-playbook -i hosts -u root playbook-by-role-docker.yml
+ansible-playbook -i hosts -u root playbook-by-role-nginx.yml
+
+# vpn only
 ansible-playbook -i hosts -u root playbook-by-role-vpn-for-ubuntu.yml
+#ansible-playbook -i hosts -u root playbook-by-role-vpn-for-centos.yml
