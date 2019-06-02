2d5340d1e744dc01f0aad31818063cd1a045e46f
Yang ChenMing <yang.chenming@oaknorth.ai>
2019-06-02 13:33:59 +0800

    add vpn scripts for both ubuntu and centos os

--- setup-digial-ocean.sh
+++ setup-digial-ocean.sh
@@ -1,5 +1,8 @@
 ansible-playbook -i hosts -u root playbook-by-role-ping.yml
 ansible-playbook -i hosts -u root playbook-by-role-basic.yml
 # ansible-playbook -i hosts -u root playbook-by-role-basic.yml -vv
-ansible-playbook -i hosts -u root playbook-by-role-docker.yml
-ansible-playbook -i hosts -u root playbook-by-role-nginx.yml
+
+# only run when needed
+# ansible-playbook -i hosts -u root playbook-by-role-docker.yml
+# ansible-playbook -i hosts -u root playbook-by-role-nginx.yml
+ansible-playbook -i hosts -u root playbook-by-role-vpn-for-ubuntu.yml
